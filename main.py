e29e7097b86159e9385212f6f87f75ff61a6c51a
zane100long <zane100long@gmail.com>
2024-06-09 14:37:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -96,8 +96,10 @@ image_path = get_holiday_image_path(holiday) if holiday else default_don
 
 drawer.draw_text_on_image(word, image_path)
 
+tweet_text=tweet_text + " " + word
+
 if (word[len(word)-2]=='e' or word[len(word)-2]=='o') and word[len(word)-1]=='r':
-        tweet_text=tweet_text + " " + word + " her? I hardly know her (I am very sorry)"
+        tweet_text=tweet_text + " her? I hardly know her (I am very sorry)"
 
 print(tweet_text)
 
